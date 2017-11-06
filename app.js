4da256513e000d3ed0136b97e69dca77d1b4d126
Charles Thomas <charles@charlesmthomas.com>
2017-11-06 18:31:02 -0500

    started seeding the tweetBank

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const nunjucks = require('nunjucks');
+
+app.set('view engine', 'html');
+app.engine('html', nunjucks.render);
+nunjucks.configure('views', { noCache: true });
+
+app.use('/', (req, res) => {
+  console.log(`${req.method} ${req.url} ${req.statusCode}`);
+
+  const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+  res.render( 'index', {title: 'Hall of Fame', people: people} );
+});
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+})
